f3b0f29d93a9311b5a778d0eee0155c89f4fc641
안성훈 <shanash511@gmail.com>
2024-06-28 13:45:50 +0900

    리액트 설정 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [boardgames, setBoardgames] = useState([]);
+  const [newGame, setNewGame] = useState({
+    name: '',
+    purchase_date: '',
+    play_count: 0,
+    fun_rating: 0,
+    sold_date: ''
+  });
+
+  useEffect(() => {
+    axios.get('/boardgames').then((response) => {
+      setBoardgames(response.data);
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewGame({ ...newGame, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/boardgames', newGame).then((response) => {
+      setBoardgames([...boardgames, { ...newGame, id: response.data.id }]);
+      setNewGame({
+        name: '',
+        purchase_date: '',
+        play_count: 0,
+        fun_rating: 0,
+        sold_date: ''
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Boardgames</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={newGame.name}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          name="purchase_date"
+          placeholder="Purchase Date"
+          value={newGame.purchase_date}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="play_count"
+          placeholder="Play Count"
+          value={newGame.play_count}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="fun_rating"
+          placeholder="Fun Rating"
+          value={newGame.fun_rating}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          name="sold_date"
+          placeholder="Sold Date"
+          value={newGame.sold_date}
+          onChange={handleChange}
+        />
+        <button type="submit">Add Game</button>
+      </form>
+      <ul>
+        {boardgames.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
